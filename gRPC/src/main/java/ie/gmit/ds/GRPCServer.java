f4af5b1f7f4c66d0ae310b6aecab1d420a5e8741
NeilK-94 <neil.kyne@gmail.com>
2019-10-25 22:55:06 +0100

    GRPC service class generated. Passwords, server and server runner classes made but not functioning yet.

---
+++ gRPC/src/main/java/ie/gmit/ds/GRPCServer.java
@@ -0,0 +1,35 @@
+package ie.gmit.ds;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class GRPCServer {
+
+    private Server server;
+    private static final Logger logger = Logger.getLogger(GRPCServer.class.getName());
+
+
+    private void start() throws IOException {
+        
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+}
---
+++ gRPC/src/main/java/ie/gmit/ds/GRPCServerRunner.java
@@ -0,0 +1,10 @@
+package ie.gmit.ds;
+
+import java.io.IOException;
+
+public class GRPCServerRunner {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		final GRPCServer grpcServer = new GRPCServer();
+		
+	}
+}
