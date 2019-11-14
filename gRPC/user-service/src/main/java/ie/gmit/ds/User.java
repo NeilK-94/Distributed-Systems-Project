55153ea65ee19de710ba1e159d7af6e83286a944
NeilK-94 <neil.kyne@gmail.com>
2019-11-14 21:35:04 +0000

    Part 2 started
    Largely adapted from the drop wizard lab. Added pom with dropwizard
    dependency and classes as well as a config.yaml file.

---
+++ gRPC/user-service/src/main/java/ie/gmit/ds/User.java
@@ -0,0 +1,47 @@
+package ie.gmit.ds;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+
+    private int userId;
+    private String userName;
+    private String email;
+    private String password;
+
+
+    public User() {
+        // Needed for Jackson deserialisation
+    }
+
+    public User(int userId, String userName, String email, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    @JsonProperty
+	public int getUserId() {
+		return userId;
+	}
+
+    @JsonProperty
+	public String getUserName() {
+		return userName;
+	}
+
+    @JsonProperty
+	public String getEmail() {
+		return email;
+	}
+
+    @JsonProperty
+	public String getPassword() {
+		return password;
+	}
+}
